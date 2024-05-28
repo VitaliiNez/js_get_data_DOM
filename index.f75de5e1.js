579f4c6935fd03465fab97c61030d02f09ae1d6b
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-05-28 22:51:28 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const populations = [
+    ...document.querySelectorAll("span.population")
+].map((population)=>population.textContent);
+const populationNode = document.querySelector("span.total-population");
+const averageNode = document.querySelector("span.average-population");
+const total = populations.reduce((initialNumber, currentNumber)=>{
+    const parsedNumber = parseInt(currentNumber.split(",").join(""));
+    return parsedNumber + initialNumber;
+}, 0);
+const average = total / populations.length;
+populationNode.innerHTML = total.toLocaleString("en-US");
+averageNode.innerHTML = average.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
